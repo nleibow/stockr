285de1719f0eeb7326e26b6a258ebf51b2ac470c
nleibow <nathan.leibowitz1@gmail.com>
2016-12-15 10:12:10 -0700

    heroku fixing good livin

--- public/js/main.js
+++ public/js/main.js
@@ -6,9 +6,15 @@ $(document).ready(function(){
 $('#searchbtn').click(function(e){
 	var stock = $("#stockLook").val();
 	var url = "https://finance.google.com/finance/info?client=ig&q=NSE:" + stock;
-	 $.get(url, function(req, res){
-		var bodyRight =(req.substr(3));
-		var answer = JSON.parse(bodyRight);
+	 $.ajax({
+            url: url, 
+            type: "GET",   
+            dataType: 'jsonp',
+            cache: false,
+            success: function(response){   
+            console.log(response);                       
+                var bodyRight =(response);
+		var answer = bodyRight;
 		var trueAnswer = {"Ticker": answer[0].t, "LastPrice": answer[0].l};
 		console.log(trueAnswer);
 		  $.post('/api', trueAnswer, function(){
@@ -20,11 +26,51 @@ $('#searchbtn').click(function(e){
 		  	console.log(tik);
 		  	var pric = trueAnswer.LastPrice;
 		  	 $('#firstTicker').append(tik);
-			$('#firstPrice').append(pric);
-		  })
+			$('#firstPrice').append(pric);                   
+            }           
+        });    
+	 // $.get(url, function(req, res){
+		// var bodyRight =(req.substr(3));
+		// var answer = JSON.parse(bodyRight);
+		// var trueAnswer = {"Ticker": answer[0].t, "LastPrice": answer[0].l};
+		// console.log(trueAnswer);
+		//   $.post('/api', trueAnswer, function(){
+		//   	console.log('somthing happened it was good');
+		  	
+		//   });
+		//   // $.get('/api', function(req, res){
+		//   	var tik =trueAnswer.Ticker;
+		//   	console.log(tik);
+		//   	var pric = trueAnswer.LastPrice;
+		//   	 $('#firstTicker').append(tik);
+		// 	$('#firstPrice').append(pric);
+		//   })
 		 
 	});
 });
+
+// $.ajax({
+//             url: url, 
+//             type: "GET",   
+//             dataType: 'jsonp',
+//             cache: false,
+//             success: function(response){                          
+//                 var bodyRight =(req.substr(3));
+// 		var answer = JSON.parse(bodyRight);
+// 		var trueAnswer = {"Ticker": answer[0].t, "LastPrice": answer[0].l};
+// 		console.log(trueAnswer);
+// 		  $.post('/api', trueAnswer, function(){
+// 		  	console.log('somthing happened it was good');
+		  	
+// 		  });
+// 		  // $.get('/api', function(req, res){
+// 		  	var tik =trueAnswer.Ticker;
+// 		  	console.log(tik);
+// 		  	var pric = trueAnswer.LastPrice;
+// 		  	 $('#firstTicker').append(tik);
+// 			$('#firstPrice').append(pric);                   
+//             }           
+//         });    
 
 
 
